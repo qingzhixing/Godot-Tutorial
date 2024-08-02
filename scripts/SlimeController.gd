d51fe402d355094b03382169e94cdf431fc03bd2
qingzhixing <qzsqqmail@qq.com>
2024-08-02 20:43:40 +0800

    翻转Slime贴图

--- scripts/SlimeController.gd
+++ scripts/SlimeController.gd
@@ -11,6 +11,7 @@ enum Direction {
 @onready var ray_cast_left = $RayCastLeft
 @onready var ray_cast_right_down = $RayCastRightDown
 @onready var ray_cast_left_down = $RayCastLeftDown
+@onready var animated_sprite = $AnimatedSprite2D
 
 var direction = Direction.FORWARD;
 
@@ -18,9 +19,11 @@ func handle_direction():
 	if (ray_cast_left.is_colliding() || (!ray_cast_left_down.is_colliding())):
 		# print("Move Forward")
 		direction = Direction.FORWARD
+		animated_sprite.flip_h = false
 	if (ray_cast_right.is_colliding() || (!ray_cast_right_down.is_colliding())):
 		# print("Move Backward")
 		direction = Direction.BACKWARD
+		animated_sprite.flip_h = true
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
