83b9ad4c44e053725f4cf91c111a5522c56d7b9a
qingzhixing <qzsqqmail@qq.com>
2024-08-03 01:29:58 +0800

    玩家跳跃音效

--- scripts/PlayerController.gd
+++ scripts/PlayerController.gd
@@ -8,6 +8,7 @@ const JUMP_VELOCITY = -320.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var jump_audio = $"Audios/Jump Audio"
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -17,6 +18,7 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_audio.play()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
