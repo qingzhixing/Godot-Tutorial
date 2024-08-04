61553f140b6bbd5886653a1c7b00c42c69d1a416
qingzhixing <qzsqqmail@qq.com>
2024-08-05 01:45:23 +0800

    为什么深拷贝了节点仍然找不到

--- scripts/ui/Heart Container.gd
+++ scripts/ui/Heart Container.gd
@@ -1,6 +1,13 @@
 extends Container
 const Heart = preload("res://scripts/ui/Heart.gd")
 const HeartResource = preload("res://prefabs/ui/heart.tscn")
+@export var heart_template: Control
+
+func deep_duplicate(node: Node) -> Node:
+	var new_node = node.duplicate()
+	for child in node.get_children(true):
+		new_node.add_child(child)
+	return new_node
 
 func clear_children():
 	var childs = get_children(false)
@@ -18,8 +25,9 @@ func generate_hearts(health: int):
 	for heart_id in range(heart_sum):
 		print("constructing heart id: ", heart_id)
 
-		var new_child_node = HeartResource.instantiate()
+		var new_child_node = deep_duplicate(heart_template)
 		print("New instance child amount:", new_child_node.get_child_count(true))
+		new_child_node.set_script(Heart)
 		var new_child = new_child_node as Heart
 
 		new_child_node.visible = true
@@ -41,6 +49,10 @@ func set_health_display(health: int):
 	print("\nsetting health: ", health)
 	# return
 	if health < 0:
+		return
+
+	if heart_template == null:
+		print("ERROR! Heart Template is null!")
 		return
 
 	clear_children()
--- scripts/ui/Heart.gd
+++ scripts/ui/Heart.gd
@@ -1,11 +1,11 @@
 extends Control
-@onready var animation_player = $":AnimationPlayer"
-@onready var heart_texture = $":Heart Texture"
+@onready var animation_player = $"AnimationPlayer"
+@onready var heart_texture = $"Heart Texture"
 #@export var animation_player: AnimationPlayer
 #@export var heart_texture: TextureRect
 
 # TODO:When instant Heart object,call these functions will lead to Errors
-#	animation_player is null
+#	Invalid call.Noneexistent function 'play' is base 'Nil'
 func set_full():
 	print("full")
 	animation_player.play("full")
