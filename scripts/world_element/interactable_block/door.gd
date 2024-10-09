22fd25283c3ac85cc37480d23fd2cc43c0b55054
qingzhixing <qzsptmail@protonmail.com>
2024-10-09 22:52:07 +0800

    door.tscn

---
+++ scripts/world_element/interactable_block/door.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+@warning_ignore("unused_parameter")
+func interact_body_enter(body: Node2D):
+	print("Next Level")
+	pass
--- scripts/world_element/interactable_block/interactable_block.gd
+++ scripts/world_element/interactable_block/interactable_block.gd
@@ -2,23 +2,29 @@ extends Node2D
 
 @export var block_collision: CollisionShape2D
 @export var interact_collision: CollisionShape2D
+@export var no_block_collision: bool = false
 
 @onready var block_body = $BlockBody
 @onready var interact_area = $InteractArea
 signal on_interact_signal(interact_body: Node2D)
 
 func _ready():
+	if interact_collision == null:
+		push_error("interact collision is null")
+	else:
+		interact_collision.get_parent().remove_child(interact_collision)
+		interact_area.add_child(interact_collision)
+		
+	if no_block_collision:
+		return
+		
 	if block_collision == null:
 		push_error("block collision is null")
 	else:
 		block_collision.get_parent().remove_child(block_collision)
 		block_body.add_child(block_collision)
 
-	if interact_collision == null:
-		push_error("interact collision is null")
-	else:
-		interact_collision.get_parent().remove_child(interact_collision)
-		interact_area.add_child(interact_collision)
+	
 
 func on_interact(interact_body: Node2D):
 	on_interact_signal.emit(interact_body)
