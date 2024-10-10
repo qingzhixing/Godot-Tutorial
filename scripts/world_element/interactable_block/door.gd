b643f56c012a9d1832f6eeaaaf69f11d0623618e
qingzhixing <qzsptmail@protonmail.com>
2024-10-10 22:32:18 +0800

    已经可以切换场景，需要研究一下跨场景的玩家数据存储

--- scripts/world_element/interactable_block/door.gd
+++ scripts/world_element/interactable_block/door.gd
@@ -2,13 +2,25 @@ extends Node2D
 
 @export var door_open: bool = false
 
+@export var target_scene: PackedScene
+
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var door_open_sfx: AudioStreamPlayer2D = $DoorOpen
 @onready var door_close_sfx: AudioStreamPlayer2D = $DoorClose
 
+var player_entered: bool = false
+
+@warning_ignore("unused_parameter")
+func _process(delta):
+	if player_entered and Input.is_action_just_pressed("interact"):
+		# 切换场景
+		get_tree().change_scene_to_packed(target_scene)
+	pass
+
 
 @warning_ignore("unused_parameter")
 func interact_body_enter(body: Node2D):
+	player_entered = true
 	if animated_sprite_2d.animation != "anim_open" and animated_sprite_2d.animation != "idle_open":
 		animated_sprite_2d.play("anim_open")
 		# 刚打开门的瞬间就有声音
@@ -17,6 +29,7 @@ func interact_body_enter(body: Node2D):
 
 @warning_ignore("unused_parameter")
 func interact_body_exit(body: Node2D):
+	player_entered = false
 	if animated_sprite_2d.animation != "anim_close" and animated_sprite_2d.animation != "idle_close":
 		animated_sprite_2d.play("anim_close")
 		door_open = false
