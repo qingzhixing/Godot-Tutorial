ddbd9b1370d7e0d82a9547138cc3627fbe1f5976
qingzhixing <qzsptmail@protonmail.com>
2024-10-11 19:17:36 +0800

    修改门动画速度以及sfx播放时刻

--- scripts/world_element/interactable_block/door.gd
+++ scripts/world_element/interactable_block/door.gd
@@ -23,7 +23,6 @@ func interact_body_enter(body: Node2D):
 	player_entered = true
 	if animated_sprite_2d.animation != "anim_open" and animated_sprite_2d.animation != "idle_open":
 		animated_sprite_2d.play("anim_open")
-		# 刚打开门的瞬间就有声音
 		door_open_sfx.play()
 		door_open = true
 
@@ -32,12 +31,11 @@ func interact_body_exit(body: Node2D):
 	player_entered = false
 	if animated_sprite_2d.animation != "anim_close" and animated_sprite_2d.animation != "idle_close":
 		animated_sprite_2d.play("anim_close")
+		door_close_sfx.play()
 		door_open = false
 
 func on_animated_sprite_animation_finished():
 	if door_open:
 		animated_sprite_2d.play("idle_open")
 	else:
-		# 门在完全关闭时发出响声
-		door_close_sfx.play()
 		animated_sprite_2d.play("idle_close")
